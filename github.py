b3f35c00f14f670e75bf29f960036bdc6fef9c25
Onurhocaoglu <onurhocaogluweb@gmail.com>
2023-02-16 15:03:47 +0300

    Github Api Added

---
+++ github.py
@@ -0,0 +1,38 @@
+import requests
+
+class Github:
+    def __init__(self):
+        self.api_url = "https://api.github.com"
+
+
+    def getUser(self, username):
+        response = requests.get(self.api_url+'/users/'+ username)
+        return response.json()
+
+    def getRepositories(self,username):
+        response = requests.get(self.api_url+'/users/'+ username + '/repos')
+        return response.json()
+
+
+github = Github()
+
+
+
+while True:
+    enter = input("1-Find User\n2-Get Repositories\n3-Exit\nSelection: ")
+
+    if enter == "1":
+        username_find = input("Username: ")
+        result = github.getUser(username_find)
+        print("*"* 30)
+        print(f"name: {result['name']}\nPublic Repos: {result['public_repos']}\nFollowing: {result['following']}\nFollowers: {result['followers']}")
+        print("*"* 30)
+    elif enter == "2":
+        repos_find = input("Username Repos: ")
+        result = github.getRepositories(repos_find)
+        for repo in result:
+            print(f"Repo Name: {repo['name']}\nRepo URL: {repo['html_url']}\nClone URL: {repo['clone_url']}")
+    elif enter == "3":
+        exit(0)
+    else:
+        print("Error!!")
---
+++ readme.md
@@ -0,0 +1,28 @@
+# GitHub Api
+
+![Github Api](github.png)
+
+## Installation
+------------------------------------------
+Öncelikle projeyi clonelayın.
+```
+git clone https://github.com/OnurHocaoglu/github_api.git
+```
+## Usage
+----------------------------------------------------
+Projeyi cloneladıktan sonra Visual Studio Code programında açınız.
+
+Linux için:
+```
+cd github_api
+code .
+```
+## Contributing
+----------------------------------------------------
+Pull requestler kabul edilir. Büyük değişiklikler için, lütfen önce neyi değiştirmek istediğinizi tartışmak için bir konu açınız.
+
+## License
+---------------------------------------------------------
+[MIT](https://choosealicense.com/licenses/mit/)
+-----------------------------------------------------
+[Onurhocaoglu.com](http://www.onurhocaoglu.com)
